e8c1199c52e8dfe51d0f795121f9c7f378db4da3
Rick Smoke <rsmoke@umich.edu>
2022-03-15 15:38:19 -0400

    add devices and configure user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'static_pages/home'
+  get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "static_pages#home"
 end
