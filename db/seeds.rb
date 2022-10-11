49348f584fbfe30ca40a6aba5fbb4784b63078e6
Margarita Barvinok <brita@umich.edu>
2022-10-11 18:48:59 -0400

    user should exist to run these commands

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke', movie: movies.first)
 
-# AppraisalType.create(name: "Payment Receipt', updated_by: "brita")
-# AppraisalType.create(name: "Comparable Value (like or comaparble item)', updated_by: "brita")
-# AppraisalType.create(name: "External Appraisal - outside vendor', updated_by: "brita")
-# AppraisalType.create(name: "Other', updated_by: "brita")
+# AppraisalType.create(name: "Payment Receipt', user_id: XXX)
+# AppraisalType.create(name: "Comparable Value (like or comaparble item)', user_id: XXX)
+# AppraisalType.create(name: "External Appraisal - outside vendor', user_id: XXX)
+# AppraisalType.create(name: "Other', user_id: XXX)
 
 # Department.create!([
-#   {name: 'American Culture', dept_number: 193000, short_name: 'Am Cult', updated_by: "brita"},
-#   {name: 'Anthro-History', dept_number: 179200, short_name: 'Anthro-Hist', updated_by: "brita"},
-#   {name: 'Anthropology', dept_number: 193000, short_name: 'Cult', updated_by: "brita"},
-#   {name: 'Applied Physics', dept_number: 184600, short_name: 'AppPhys', updated_by: "brita"},
-#   {name: 'Asian Languages & Cultures', dept_number: 176000, short_name: 'Asian', updated_by: "brita"}
+#   {name: 'American Culture', dept_number: 193000, short_name: 'Am Cult', user_id: XXX},
+#   {name: 'Anthro-History', dept_number: 179200, short_name: 'Anthro-Hist', user_id: XXX},
+#   {name: 'Anthropology', dept_number: 193000, short_name: 'Cult', user_id: XXX},
+#   {name: 'Applied Physics', dept_number: 184600, short_name: 'AppPhys', user_id: XXX},
+#   {name: 'Asian Languages & Cultures', dept_number: 176000, short_name: 'Asian', user_id: XXX}
 # ])
