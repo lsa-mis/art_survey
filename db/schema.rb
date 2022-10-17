e2396cc1d390dba0d3382b1125179e0adb885665
Rick Smoke <rsmoke@umich.edu>
2022-10-17 18:04:57 -0400

    Remove uniqname from Permissions table
    The field was hidden in the diagram but showed up in the schema script.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_214500) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_220342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,7 +104,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_214500) do
   end
 
   create_table "permissions", force: :cascade do |t|
-    t.string "uniqname", null: false
     t.bigint "role_id", null: false
     t.bigint "department_id", null: false
     t.integer "updated_by"
