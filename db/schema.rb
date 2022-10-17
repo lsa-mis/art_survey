3ac9277bc5e98dd23b55ad3e1a7f8ecbf4b2cef9
Rick Smoke <rsmoke@umich.edu>
2022-10-17 09:43:38 -0400

    Add SAML specific fields to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_222641) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_134151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_222641) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "uniqname"
+    t.string "principal_name"
+    t.string "display_name"
+    t.string "person_affiliation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
