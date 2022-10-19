1553b05f32b1dbdd82753a428eb8b0f58550ef9d
Rick Smoke <rsmoke@umich.edu>
2022-10-19 18:07:50 -0400

    Check for presence of data to avoid errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def index
-    @home_message = PageInformation.find_by(location: "home").content
+    @home_message = PageInformation.find_by(location: "home").content  if StaticPage.find_by(location: 'home').present?
   end
 
   def about
-    @about_message = PageInformation.find_by(location: "about").content
+    @about_message = PageInformation.find_by(location: "about").content  if StaticPage.find_by(location: 'about').present?
   end
 
 end
