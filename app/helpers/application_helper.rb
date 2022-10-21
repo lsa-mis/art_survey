b463777383728738550875638208172e5281fe75
Margarita Barvinok <brita@umich.edu>
2022-10-21 18:06:13 -0400

    add svg file and method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def svg(svg)
+    file_path = "app/assets/images/svg/#{svg}.svg"
+    return File.read(file_path).html_safe if File.exist?(file_path)
+    file_path
+  end
+  
 end
