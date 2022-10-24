acdf02ff58d8872bf5d220e5931ac42187cbc36f
Margarita Barvinok <brita@umich.edu>
2022-10-24 11:54:55 -0400

    edit value_cost validation in js controller

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -5,32 +5,24 @@ export default class extends Controller {
   search() {
     clearTimeout(this.timeout)
 
-    const MIN = parseInt(0)
-    const MAX = parseInt(100000)
-
     this.timeout = setTimeout(() => {
       var min_value_cost = parseInt(this.min_value_costTarget.value)
       var max_value_cost = parseInt(this.max_value_costTarget.value)
 
       if (Number.isNaN(min_value_cost)) { min_value_cost = 0 }
-      if (Number.isNaN(max_value_cost)) { max_value_cost = 0 }
+      if (Number.isNaN(max_value_cost)) { max_value_cost = min_value_cost }
 
-      if (min_value_cost != 0 || max_value_cost != MAX) {
+      if (min_value_cost != 0 || max_value_cost != 0) {
 
         if (min_value_cost < 0) {
           this.value_cost_errorTarget.classList.add("value_cost-error--display")
           this.value_cost_errorTarget.classList.remove("value_cost-error--hide")
-          this.value_cost_errorTarget.innerText = "Min should be positive"
-        }
-        else if (max_value_cost > MAX) {
-          this.value_cost_errorTarget.classList.add("value_cost-error--display")
-          this.value_cost_errorTarget.classList.remove("value_cost-error--hide")
-          this.value_cost_errorTarget.innerText = "Max should be less then " + MAX
+          this.value_cost_errorTarget.innerText = "From should be positive"
         }
         else if (min_value_cost > max_value_cost) {
           this.value_cost_errorTarget.classList.add("value_cost-error--display")
           this.value_cost_errorTarget.classList.remove("value_cost-error--hide")
-          this.value_cost_errorTarget.innerText = "Min should be smaller than Max"
+          this.value_cost_errorTarget.innerText = "From should be smaller than To"
         }
         else {
           this.value_cost_errorTarget.classList.add("value_cost-error--hide")
