8eeebdf726e33d756393f69a8cf82fd2196d7bfb
Rick Smoke <rsmoke@umich.edu>
2022-10-25 17:34:38 -0400

    Run annotate on PageInfo

--- spec/factories/page_informations.rb
+++ spec/factories/page_informations.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: page_informations
+#
+#  id         :bigint           not null, primary key
+#  location   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :page_information do
     location { "MyString" }
--- spec/models/page_information_spec.rb
+++ spec/models/page_information_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: page_informations
+#
+#  id         :bigint           not null, primary key
+#  location   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe PageInformation, type: :model do
