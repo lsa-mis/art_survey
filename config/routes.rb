bcdb8993760103905ce34c3d36c95898b212d7a9
Ananta Saple <anantas@umich.edu>
2022-10-26 13:18:05 -0400

    LRA-414 Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,14 @@ Rails.application.routes.draw do
   resources :departments
   resources :appraisal_types
   resources :roles
-  devise_for :users
+
   get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "static_pages#home"
+
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions"} do
+    delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
 end
