51dee19dd0c7a34cd9e6db4c8d81095403570a5e
Ananta Saple <anantas@umich.edu>
2022-11-09 09:30:00 -0500

    LRA-444 Stimulus controller changes

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    this.element.style.animation = "fade-in-and-out 4s"
-    setTimeout(() => { this.element.remove() }, 4000)
+    setTimeout(() => { this.dismiss(); }, 5000)
+  }
+  dismiss() {
+    this.element.remove();
   }
 }
