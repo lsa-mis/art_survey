e41c60904d535d464208c4623f84793f7a9d0ae3
Rick Smoke <rsmoke@umich.edu>
2022-11-17 17:43:49 -0500

    Add methods that define role and access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,50 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+
+  def superuser_role_object
+    Role.find_by(title: "SuperUser")
+  end
+
+  def department_admin_role_object
+    Role.find_by(title: "Department Administrator")
+  end
+
+  def recorder_role_object
+    Role.find_by(title: "Recorder")
+  end
+
+  def is_super_user?
+    #current_user.uniqname has an access that includes the role superuser
+    if Permission.where(role_id: superuser_role_object).present?
+      permission_collection = Permission.where(role_id: superuser_role_object)
+      Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
+    end
+  end
+  helper_method :is_super_user?
+
+  def is_department_admin?
+    #current_user.uniqname has an access that is a department_admin
+    if Permission.where(role_id: department_admin_role_object).present?
+      permission_collection = Permission.where(role_id: department_admin_role_object)
+      Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
+    end
+  end
+  helper_method :is_department_admin?
+
+  def is_recorder?
+    #current_user.uniqname has an access that is a recorder
+    if Permission.where(role_id: recorder_role_object).present?
+      permission_collection = Permission.where(role_id: recorder_role_object)
+      Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
+    end
+  end
+  helper_method :is_recorder?
+
+  def is_access_authorized?
+    unless is_super_user? || is_department_admin? || is_recorder?
+      redirect_to root_path
+    end
+  end
+  helper_method :is_access_authorized?
 end
