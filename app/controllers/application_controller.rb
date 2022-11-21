40292dc907c995534e53481b98a6285aa3f6c3dc
Rick Smoke <rsmoke@umich.edu>
2022-11-21 11:36:15 -0500

    Cleaned up methods in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,51 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-
-  def superuser_role_object
-    Role.find_by(title: "SuperUser")
+  def role_object( title )
+    Role.find_by(title: title)
   end
-  helper_method :superuser_role_object
+  # helper_method :role_object
 
-  def department_admin_role_object
-    Role.find_by(title: "Department Administrator")
-  end
-  helper_method :department_admin_role_object
-
-  def recorder_role_object
-    Role.find_by(title: "Recorder")
-  end
-  helper_method :recorder_role_object
-
-  def is_super_user?
-    #current_user.uniqname has an access that includes the role superuser
-    if Permission.where(role_id: superuser_role_object).present?
-      permission_collection = Permission.where(role_id: superuser_role_object)
+  def is_user?(role)
+    if Permission.where(role_id: role).present?
+      permission_collection = Permission.where(role_id: role)
       Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
     end
   end
-  helper_method :is_super_user?
-
-  def is_department_admin?
-    #current_user.uniqname has an access that is a department_admin
-    if Permission.where(role_id: department_admin_role_object).present?
-      permission_collection = Permission.where(role_id: department_admin_role_object)
-      Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
-    end
-  end
-  helper_method :is_department_admin?
-
-  def is_recorder?
-    #current_user.uniqname has an access that is a recorder
-    if Permission.where(role_id: recorder_role_object).present?
-      permission_collection = Permission.where(role_id: recorder_role_object)
-      Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
-    end
-  end
-  helper_method :is_recorder?
+  helper_method :is_user?
 
   def is_access_authorized?
-    unless is_super_user? || is_department_admin? || is_recorder?
+    unless is_user?(role_object("SuperUser")) || is_user?(role_object("Department Administrator")) || is_user?(role_object("Recorder"))
       redirect_to root_path
     end
   end
