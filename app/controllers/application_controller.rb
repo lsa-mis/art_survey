1490c36e0533a2d01317b6a7c93fecc575a8396f
Rick Smoke <rsmoke@umich.edu>
2022-11-29 08:21:49 -0500

    Update role methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,26 @@ class ApplicationController < ActionController::Base
   def role_object( title )
     Role.find_by(title: title)
   end
-  # helper_method :role_object
+  helper_method :role_object
 
-  def is_user?(role)
-    if Permission.where(role_id: role).present?
+  def is_user_a?(role)
+    if Permission.where(role_id: role).exists?
       permission_collection = Permission.where(role_id: role)
       Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
     end
   end
-  helper_method :is_user?
-
-  def is_access_authorized?
-    if is_user?(role_object("SuperUser")) || is_user?(role_object("Department Administrator")) || is_user?(role_object("Recorder"))
+  helper_method :is_user_a?(role)
+  
+  def is_super_user!
+    if is_user?("SuperUser")
       true
-    else 
-      false
     end
   end
-  helper_method :is_access_authorized?
+  helper_method :is_super_user!
+
+  def access_authorized!
+    Access.all.pluck(:uniqname).include?(session[:user_uniqname])
+      true
+  end
+  helper_method :access_authorized!
 end
