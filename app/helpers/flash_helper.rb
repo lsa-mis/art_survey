bb7271eea50d55b7f252fc7240092283fd084eb1
Rick Smoke <rsmoke@umich.edu>
2022-11-30 15:26:17 -0500

    change error typre to alert since alert is a Rails idiom

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,7 +1,7 @@
 module FlashHelper
   def css_class_for_flash(type)
     case type.to_sym
-    when :error
+    when :alert
       "bg-red-100 text-red-700"
     else
       "bg-green-100 text-green-700"
