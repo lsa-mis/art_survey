5adb164ba22b5b65be5c843b595c394d1da3622c
Rick Smoke <rsmoke@umich.edu>
2022-12-14 16:41:09 -0500

    Remove unused helper_method definitions
    Refactor the authorization and DRY up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   def role_object( title )
     Role.find_by(title: title)
   end
-  helper_method :role_object
 
   def is_user_a?( role_title )
     permission_collection = Permission.where(role_id: role_object( role_title ))
@@ -12,7 +11,6 @@ class ApplicationController < ActionController::Base
       Access.where(permission_id: permission_collection).pluck(:uniqname).include?(session[:user_uniqname])
     end
   end
-  helper_method :is_user_a?
   
   def is_super_user!
     if is_user_a?("SuperUser")
@@ -39,7 +37,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_path unless access_authorized!
     flash.alert = "Not Authorized." unless access_authorized!
   end
-  helper_method :check_for_authorized_access
 
   def super_user_access_authorized!
     redirect_to root_path unless is_super_user!
@@ -52,23 +49,47 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user_access
-    Access.where(uniqname: session[:user_uniqname]).pluck(:permission_id)
+    Access.where(uniqname: session[:user_uniqname])
+  end
+
+  def current_department_admin_user_department_ids
+    department_admin_permissions_collection = Permission.where(role_id: role_object( "Department Administrator" ))
+    current_department_admin_user_access = Access.where(permission_id: department_admin_permissions_collection, uniqname: session[:user_uniqname])
+    current_department_admin_user_department_ids = Permission.find(current_department_admin_user_access.pluck(:permission_id)).pluck(:department_id)
+  end
+
+  def current_department_admin_user_department_permissions_collection
+    Permission.where(department_id: current_department_admin_user_department_ids)
+  end
+
+  def current_user_permissions
+    if is_super_user!
+      Permission.all
+    elsif is_department_admin_user!
+      current_department_admin_user_department_permissions_collection
+    else
+      Permission.where(id: current_user_access.pluck(:permission_id))
+    end
   end
 
   def current_user_departments
     if is_super_user!
       Department.all
+    elsif is_department_admin_user!
+      permission_collection = Permission.where(id: current_user_access.pluck(:permission_id))
+      Department.where(id: (current_department_admin_user_department_ids.concat(permission_collection.pluck(:department_id))).uniq)
     else
-      Department.where(id: Permission.where(id: current_user_access).pluck(:department_id).uniq)
+      Department.where(id: current_user_permissions.pluck(:department_id)).uniq
     end
-  end
-  helper_method :current_user_departments
-
-  def current_user_permissions
-    Permission.where(department_id: current_user_departments)
   end
 
   def get_accesses_collection
-    Access.where(permission_id: current_user_permissions)
+    if is_super_user!
+      Access.all
+    elsif is_department_admin_user!
+      department_admin_access_collection = (Access.where(permission_id: current_department_admin_user_department_permissions_collection)).uniq
+    else
+      current_user_access
+    end
   end
 end
