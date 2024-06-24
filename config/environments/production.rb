3e685d30dc98bd88ec54631fcf27d6c6661b0b1d
rsmokeUM <rsmoke@umich.edu>
2024-06-24 12:16:53 -0400

    chore: Enable SSL and secure session cookies in production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,10 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+
+  # Ensure the session cookies are also set to secure in production
+  Rails.application.config.session_store :cookie_store, key: '_art_survey_session', secure: Rails.env.production?
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
