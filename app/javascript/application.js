2b597d1bbdb0d6d8e7d3dfca2828c02ed565318c
rsmokeUM <rsmoke@umich.edu>
2025-02-20 17:44:52 -0500

    Update ActionText import and add comments in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "controllers"  // This imports controllers/index.js
+
+// Import Trix and ActionText in the correct order
 import "trix"
-import "@rails/actiontext"
+import * as ActionText from "@rails/actiontext"
 
 import Swiper, { Navigation } from "swiper";
 Swiper.use([Navigation]);
