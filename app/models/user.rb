50fb2dfce3cdebe4b2f57d4367a2d95469da717f
rsmokeUM <rsmoke@umich.edu>
2025-02-24 11:33:42 -0500

    Remove commented Devise module documentation
    - Clean up unnecessary comments in User model
    - Retain core Devise authentication configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,6 @@
 #  person_affiliation     :string
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :omniauthable, omniauth_providers: [:saml]
