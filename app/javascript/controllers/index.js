a656f4a3cc8b4b20331cdc12f155fc88be3b1419
rsmokeUM <rsmoke@umich.edu>
2025-07-28 10:17:11 -0400

    Add CSV gem and implement DownloadCsvController
    - Added the `csv` gem to the Gemfile for CSV handling.
    - Created `DownloadCsvController` to manage CSV download functionality.
    - Updated the `art_items` view to include a department selection dropdown and a button for downloading CSV reports, utilizing Stimulus for interactivity.
    - Removed inline JavaScript for CSV download in favor of the new controller for cleaner code and better maintainability.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+// import DownloadCsvController from "./download_csv_controller"
+// application.register("download-csv", DownloadCsvController)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
