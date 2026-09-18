5a47b2b20f7108ca722de851b926f93de55683a5
Margarita Barvinok <brita@umich.edu>
2026-09-18 12:21:34 -0400

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Highlight code that enqueued background job in logs.
+  config.active_job.queue_adapter = :async
   config.active_job.verbose_enqueue_logs = true
 
   # Suppress logger output for asset requests.
