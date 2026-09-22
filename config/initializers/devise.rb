e65f5ffcb57ab74efb21f61f2fed7bc2d76bdb1e
Margarita Barvinok <brita@umich.edu>
2026-09-22 13:21:09 -0400

    edit devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,14 +13,12 @@ Devise.setup do |config|
   service_provider_private_key = Rails.application.credentials.service_provider_private_key
   service_provider_certificate = Rails.application.credentials.service_provider_certificate
 
-  if Rails.env.development?
-    consumer_service_url = Rails.application.credentials.dev_assertion_consumer_service_url
-    entity_id = Rails.application.credentials.dev_entity_id
-    idp_sso_service_url = Rails.application.credentials.dev_idp_sso_service_url
-    idp_logout_url = Rails.application.credentials.dev_idp_slo_target_url
-    idp_entity_id = Rails.application.credentials.dev_idp_entity_id
-    idp_cert = Rails.application.credentials.dig(:okta, :dev_idp_cert)
-  end
+  consumer_service_url = Rails.application.credentials.dev_assertion_consumer_service_url
+  entity_id = Rails.application.credentials.dev_entity_id
+  idp_sso_service_url = Rails.application.credentials.dev_idp_sso_service_url
+  idp_logout_url = Rails.application.credentials.dev_idp_slo_target_url
+  idp_entity_id = Rails.application.credentials.dev_idp_entity_id
+  idp_cert = Rails.application.credentials.dig(:okta, :dev_idp_cert)
 
   if Rails.env.staging?
     consumer_service_url = Rails.application.credentials.staging_assertion_consumer_service_url
